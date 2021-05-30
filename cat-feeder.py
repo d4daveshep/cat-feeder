216f2cedef38942824fc766beacf9d27e5553c9d
d4daveshep <34198977+d4daveshep@users.noreply.github.com>
2021-05-30 15:49:21 +1200

    Add files via upload

---
+++ cat-feeder.py
@@ -0,0 +1,26 @@
+# This file is the top level programme control for the cat-feeder
+
+### Initialise
+
+# Servo parameters
+Period = 1/50
+
+
+# Wait for email instructions
+
+# Parse email instructions
+
+# Do instructions
+
+# Handle any errors
+
+# Email response
+
+# Tidy up
+
+### Utility functions ###
+
+# Calculate servo duty cycle from desired angle
+def calculateDutyCycle( angle ) :
+
+
