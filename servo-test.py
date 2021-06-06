bd081becb49d69c13513cc59667e546751f89d78
d4daveshep <david@daveshep.net.nz>
2021-06-07 11:05:45 +1200

    testing jaycar servo

--- servo-test.py
+++ servo-test.py
@@ -25,9 +25,9 @@ servo1.ChangeDutyCycle(dutycycle(1.5))
 time.sleep(0.5)
 
 # Test end points
-servo1.ChangeDutyCycle(dutycycle(0.64))
+servo1.ChangeDutyCycle(dutycycle(0.5))
 time.sleep(1.0)
-servo1.ChangeDutyCycle(dutycycle(2.36))
+servo1.ChangeDutyCycle(dutycycle(2.2))
 time.sleep(1.0)
 
 # Test mid point
