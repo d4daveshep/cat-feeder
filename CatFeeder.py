653887eb4698fd26f3b705d1f9b80c90747eda31
d4daveshep <david@daveshep.net.nz>
2021-06-08 09:12:45 +1200

    added logging

--- CatFeeder.py
+++ CatFeeder.py
@@ -22,7 +22,7 @@ def feedByGmail():
     logging.info("checking email")
     gmailWrapper = GmailWrapper(HOSTNAME, USERNAME, PASSWORD)
     ids = gmailWrapper.getIdsBySubject('feed cats')
-    logging.info("got %d email", ids.len())
+    logging.info("got %d email", len(ids))
     if (len(ids) > 0):
         try:
             feed()
