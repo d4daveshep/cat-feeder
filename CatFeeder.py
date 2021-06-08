84a1c0744bc851f7fe16d331c8329283a2c05494
d4daveshep <david@daveshep.net.nz>
2021-06-08 12:00:01 +1200

    added logging

--- CatFeeder.py
+++ CatFeeder.py
@@ -1,17 +1,49 @@
 #!/usr/bin/env python
 
 from GmailWrapper import GmailWrapper
+from datetime import datetime
 
 import RPi.GPIO as GPIO
 import time
 import logging
+import os
 
 HOSTNAME = 'imap.gmail.com'
 USERNAME = 'cat-feeder@daveshep.net.nz'
 PASSWORD = 'vhov zueo fyas bkxd'
 SUBJECT_FEED = 'FEED CATS'  # case insensitive
+SUBJECT_PHOTO = 'TAKE PHOTO'
 
 GPIO_PIN = 11
+
+WORKING_DIRECTORY = '/home/pi/dev/cat-feeder/'
+
+
+# check emails for a photo request, take webcam photo and send as reply
+def sendPhoto():
+    logging.info("checking email at " + USERNAME)
+    gmailWrapper = GmailWrapper(HOSTNAME, USERNAME, PASSWORD)
+    photoEmails = gmailWrapper.getIdsBySubject(SUBJECT_PHOTO)
+    if (len(photoEmails) > 0):
+        logging.info("got %d email to " + SUBJECT_PHOTO, len(photoEmails))
+        try:
+            # make filename with timestamp
+            dateTimeObj = datetime.now()
+            timestampStr = dateTimeObj.strftime("%Y%m%d-%H%M")
+            photoFilename = WORKING_DIRECTORY + timestampStr + '.jpg'
+
+            # take photo
+            os.system('fswebcam --jpeg 95 --save ' + photoFilename)
+
+            # send as email attachment
+
+            logging.info('saved' + photoFilename)
+
+        except Exception as e:
+            logging.error("FAILED to" + SUBJECT_PHOTO, "%s", e)
+
+    else:
+        logging.info("no photos to take")
 
 
 # dutycycle calculation for Jaycar YM2763 servo
@@ -32,7 +64,7 @@ def feedByGmail():
         except Exception as e:
             logging.error("FAILED to" + SUBJECT_FEED, "%s", e)
     else:
-        logging.info("nothing to do")
+        logging.info("no feeding to do")
 
 
 def feed():
@@ -60,7 +92,6 @@ def feed():
         servo.ChangeDutyCycle(dutycycle(0.0))
         time.sleep(0.5)
 
-
     finally:
         # always cleanup after ourselves
         servo.stop()
@@ -69,7 +100,7 @@ def feed():
 
 if __name__ == '__main__':
     # configure logging
-    logging.basicConfig(filename="feeder.log",
+    logging.basicConfig(filename=WORKING_DIRECTORY + "feeder.log",
                         level=logging.INFO,
                         format='%(asctime)s %(message)s', datefmt='%d-%m-%Y %H:%M:%S')
 
