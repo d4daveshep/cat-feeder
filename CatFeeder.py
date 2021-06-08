148a5be4af67c0a814e3557e0ab2da16ec2ca053
d4daveshep <david@daveshep.net.nz>
2021-06-08 19:54:09 +1200

    added photo email function

--- CatFeeder.py
+++ CatFeeder.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
-import email
 
-from GmailWrapper import GmailWrapper
+import logging
+import os
+import time
 from datetime import datetime
 
 import RPi.GPIO as GPIO
-import time
-import logging
-import os
+
+from GmailWrapper import GmailWrapper
 
 HOSTNAME = 'imap.gmail.com'
 USERNAME = 'cat-feeder@daveshep.net.nz'
@@ -23,58 +23,59 @@ WORKING_DIRECTORY = '/home/pi/dev/cat-feeder/'
 # check emails for a photo request, take webcam photo and send as reply
 def sendPhoto():
     logging.info("checking email at " + USERNAME)
-    gmailWrapper = GmailWrapper(HOSTNAME, USERNAME, PASSWORD)
-    photoEmails = gmailWrapper.getIdsBySubject(SUBJECT_PHOTO)
-    if (len(photoEmails) > 0):
-        logging.info("got %d email to " + SUBJECT_PHOTO, len(photoEmails))
+    gmail_wrapper = GmailWrapper(HOSTNAME, USERNAME, PASSWORD)
+    photo_emails = gmail_wrapper.getIdsBySubject(SUBJECT_PHOTO)
+    if len(photo_emails) == 0:
+        logging.info('no photos to take')
+
+    else:
+        logging.info("got %d email to " + SUBJECT_PHOTO, len(photo_emails))
         try:
             # make filename with timestamp
-            dateTimeObj = datetime.now()
-            timestampStr = dateTimeObj.strftime("%Y%m%d-%H%M")
-            photoFilename = WORKING_DIRECTORY + timestampStr + '.jpg'
+            date_time_obj = datetime.now()
+            timestamp = date_time_obj.strftime("%Y%m%d-%H%M")
+            photo_filename = WORKING_DIRECTORY + timestamp + '.jpg'
 
             # take photo
-            os.system('fswebcam --jpeg 95 --save ' + photoFilename)
-            logging.info('saved ' + photoFilename)
+            os.system('fswebcam --jpeg 95 --save ' + photo_filename)
+            logging.info('saved ' + photo_filename)
 
             # get reply address
-            reply_address = gmailWrapper.getReplyTo(photoEmails[0])
+            reply_address = gmail_wrapper.getReplyTo(photo_emails[0])
             logging.info('reply address is ' + reply_address)
 
             # send as email attachment
-            gmailWrapper.sendImagefile('cat feeder photo at ' + timestampStr, reply_address, photoFilename)
+            gmail_wrapper.sendImagefile('cat feeder photo at ' + timestamp, reply_address, photo_filename)
             logging.info('email sent')
 
         except Exception as e:
-            logging.error("FAILED to " + SUBJECT_PHOTO, "%s", e)
-
-    else:
-        logging.info("no photos to take")
+            logging.error('FAILED to ' + SUBJECT_PHOTO, '%s', e)
 
 
 # dutycycle calculation for Jaycar YM2763 servo
 def dutycycle(angle):
-    return ((angle + 90) / 20.0 + 3.0)  # -90.0 < angle < +90.0
+    return (angle + 90) / 20.0 + 3.0  # -90.0 < angle < +90.0
 
 
 def feedByGmail():
-    logging.info("checking email at " + USERNAME)
-    gmailWrapper = GmailWrapper(HOSTNAME, USERNAME, PASSWORD)
-    feedEmails = gmailWrapper.getIdsBySubject(SUBJECT_FEED)
-    if (len(feedEmails) > 0):
-        logging.info("got %d email to " + SUBJECT_FEED, len(feedEmails))
+    logging.info('checking email at ' + USERNAME)
+    gmail_wrapper = GmailWrapper(HOSTNAME, USERNAME, PASSWORD)
+    feed_emails = gmail_wrapper.getIdsBySubject(SUBJECT_FEED)
+    if len(feed_emails) == 0:
+        logging.info('no feeding to do')
+    else:
+        logging.info('got %d email to ' + SUBJECT_FEED, len(feed_emails))
         try:
             feed()
-            gmailWrapper.markAsRead(feedEmails)
-            logging.info("completed successfully")
+            gmail_wrapper.markAsRead(feed_emails)
+            logging.info('completed successfully')
         except Exception as e:
-            logging.error("FAILED to" + SUBJECT_FEED, "%s", e)
-    else:
-        logging.info("no feeding to do")
+            logging.error('FAILED to' + SUBJECT_FEED, '%s', e)
 
 
 def feed():
     # let the GPIO library know where we've connected our servo to the Pi
+    global servo
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(GPIO_PIN, GPIO.OUT)
 
@@ -106,7 +107,7 @@ def feed():
 
 if __name__ == '__main__':
     # configure logging
-    logging.basicConfig(filename=WORKING_DIRECTORY + "feeder.log",
+    logging.basicConfig(filename=WORKING_DIRECTORY + 'feeder.log',
                         level=logging.INFO,
                         format='%(asctime)s %(message)s', datefmt='%d-%m-%Y %H:%M:%S')
 
@@ -118,6 +119,3 @@ if __name__ == '__main__':
 
     # kick off the feeding process (move the servo)
 #    feed()
-
-
-# This file is the top level programme control for the cat-feeder
--- GmailWrapper.py
+++ GmailWrapper.py
@@ -31,30 +31,30 @@ class GmailWrapper:
 
     #   The IMAPClient search returns a list of Id's that match the given criteria.
     #   An Id in this case identifies a specific email
-    def getIdsBySubject(self, subject, unreadOnly=True, folder='INBOX'):
+    def getIdsBySubject(self, subject, unreadonly=True, folder='INBOX'):
         #   search within the specified folder, e.g. Inbox
         self.setFolder(folder)
 
         #   build the search criteria (e.g. unread emails with the given subject)
         self.searchCriteria = [UNSEEN_FLAG, 'SUBJECT', subject]
 
-        if unreadOnly == False:
+        if unreadonly is False:
             #   force the search to include "read" emails too
             self.searchCriteria.append(SEEN_FLAG)
 
         #   conduct the search and return the resulting Ids
         return self.server.search(self.searchCriteria)
 
-    def markAsRead(self, mailIds, folder='INBOX'):
+    def markAsRead(self, mail_ids, folder='INBOX'):
         self.setFolder(folder)
-        self.server.set_flags(mailIds, [SEEN])
+        self.server.set_flags(mail_ids, [SEEN])
 
     def setFolder(self, folder):
         self.server.select_folder(folder)
 
-    def getReplyTo(self, messageID, folder='INBOX'):
+    def getReplyTo(self, message_id, folder='INBOX'):
         self.setFolder(folder)
-        message_data = self.server.fetch([messageID], 'RFC822').get(messageID)
+        message_data = self.server.fetch([message_id], 'RFC822').get(message_id)
         email_message = email.message_from_bytes(message_data[b'RFC822'])
         email_return_path = email_message.get('Return-Path')
         address = email_return_path[1:len(email_return_path) - 1]
@@ -63,27 +63,31 @@ class GmailWrapper:
         # message = email.message_from_bytes(message_data)
         # return message_data
 
-    def sendImagefile(self, subject, address, filename):
-        # create email object that has multiple part:
+    def sendImagefile(self, subject, to_address, filename):
+
+        # create email object that has multiple part
         msg = MIMEMultipart()
         msg['From'] = self.userName
-        msg['To'] = address
+        msg['To'] = to_address
         msg['Subject'] = Header(subject, 'utf-8').encode()
 
+        # add message content
         msg_content = MIMEText('photo attached is ' + os.path.basename(filename), 'plain', 'utf-8')
         msg.attach(msg_content)
 
+        # read the image file
         fp = open(filename, 'rb')
         img = MIMEImage(fp.read())
         fp.close()
 
+        # add the image
         img.add_header('Content-Disposition', 'attachment; filename=' + os.path.basename(filename))
-        #img.add_header()
         msg.attach(img)
 
+        # send message securely
         smtp = smtplib.SMTP_SSL('smtp.gmail.com', 465)
         smtp.login(self.userName, self.password)
-        smtp.sendmail(self.userName, address, msg.as_string())
+        smtp.sendmail(self.userName, to_address, msg.as_string())
         smtp.quit()
 
 
