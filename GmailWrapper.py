5bd6a4f18b1d1d29fca76e44ee062d9575e2a4fa
d4daveshep <david@daveshep.net.nz>
2021-06-08 15:08:26 +1200

    added photo email function

--- GmailWrapper.py
+++ GmailWrapper.py
@@ -48,7 +48,7 @@ class GmailWrapper:
     def getReplyTo(self, messageID, folder='INBOX'):
         self.setFolder(folder)
         message_data = self.server.fetch([messageID], 'RFC822')
-        message = email.message_from_bytes(message_data)
-        return message
+        #message = email.message_from_bytes(message_data)
+        return message_data
 
 #    def sendImagefile(self, filename):
